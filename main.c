57c69f14f9e1565c3348dc82c0a0e286c32164a9
JH Baek <zirubak@gmail.com>
2014-06-23 23:42:32 -0700

    linked list

--- main.c
+++ main.c
@@ -15,7 +15,6 @@ typedef struct _node{
 
 
 //to init linked list
-
 node *head, *tail; //define by global variable
 
 void init_list(void)
@@ -96,11 +95,101 @@ int delete_node(int k)
 	}
 }
 
+// after finding value if the value is existed add the node
+// add t in front of k
+node *insert_node(int t, int k)
+{
+	node *s; // point for searching
+	node *p; // point to point previous of s
+	node *r; // point to insert value
 
+	p = head;
+	s = p->next;
+
+	//finding value of key
+	while( s->key != k && s != tail)
+	{
+		p = p->next;
+		s = p->next;
+	}
+
+	if(s != tail)
+	{
+		r = (node*)malloc(sizeof(node));
+		r->key = t;
+		p->next = r;
+		r->next = s;
+	}
+
+	return p->next;
+}
+
+node *ordered_insert(int k)
+{
+	node *s;
+	node *p;
+	node *r;
+
+	p = head;
+	s = p->next;
+
+	while( s->key <= k && s != tail)
+	{
+		p = p->next;
+		s = s->next;
+	}
+
+	r = (node*)malloc(sizeof(node));
+	r->key = k;
+	p->next = r;
+	s->next = s;
+
+	return r;
+}
+
+// print list
+void print_list(node *t)
+{
+	printf("\n");
+	while(t != tail)
+	{
+		printf("\n %d", t->key);
+		t = t->next;
+	}
+
+}
+
+//delete all
+node *delete_all(void)
+{
+	node *s;
+	node *t;
+
+	t = head->next;
+
+	while(t != tail)
+	{
+		s = t;
+		t = t->next;
+		free(s);
+	}
+
+	head->next = tail;
+	return head;
+}
 
 
 int main(void)
 {
+
+	node *t;
+
+	init_list();
+	ordered_insert(10);
+	ordered_insert(5);
+
+	printf("\nInitial Linked list is ");
+	print_list(head->next);
 
 	return 0;
 }
