3b90b67aba31e510250d064d0e6aee6bc359a6f3
JH Baek <zirubak@gmail.com>
2014-06-25 14:20:52 -0700

    simple linked list example Page 136
    c로 배우는 알고리즘 중.
    
    
    Signed-off-by: JH Baek <zirubak@gmail.com>

--- main.c
+++ main.c
@@ -1,38 +1,79 @@
-/*
- * main.c
- *
- *  Created on: Jun 23, 2014
- *      Author: jh
- */
+// simple linked list example
+// page 136
+
 
 #include <stdio.h>
 
-//data structure of simple linked list
 typedef struct _node{
-	int key; //save data
-	struct _node *next; // the point of next node
+	int key;
+	struct _node *next;
 } node;
 
-
-//to init linked list
-
-node *head, *tail; //define by global variable
+node *head, *tail;
 
 void init_list(void)
 {
-	head = (node*)malloc(sizeof(node)); // make space of head
-	tail = (node*)malloc(sizeof(node)); // make space of tail
+	head = (node*)malloc(sizeof(node));
+	tail = (node*)malloc(sizeof(node));
 
 	head->next = tail;
 	tail->next = tail;
 }
 
+node *ordered_insert(int k)
+{
+	node *s;  //현재의 node
+	node *p;  //이전의 node
+	node *r;  //삽입되는 node
 
+	p = head;
+	s = p->next;
 
+	while(s->key <= k && s != tail)
+	{
+		p = p->next;
+		s = s->next;
+	}
+
+	r = (node*)malloc(sizeof(node));
+	r->key = k;
+	p->next = r;
+	r->next = s;
+
+	return r;
+}
+
+void print_list(node *t)
+{
+	printf("\n");
+	while(t != tail)
+	{
+		printf("%-8d", t->key);
+		t = t->next;
+	}
+}
 
 int main(void)
 {
+	node *t;
+
+	init_list();
+
+//	순서를 유지하면서 삽입
+	ordered_insert(10);
+	ordered_insert(5);
+	ordered_insert(8);
+	ordered_insert(3);
+	ordered_insert(1);
+	ordered_insert(7);
+	ordered_insert(8);
+
+	printf("\nInitial Linked list is ");
+	print_list(head->next);
+
+
+
+
 
 	return 0;
 }
-
