c82c89a16ba5d7badf49c2e24cbe032ab991e5a0
JH Baek <zirubak@gmail.com>
2014-06-25 22:21:55 -0700

    simple linked list example Page 136
    c로 배우는 알고리즘 중.
    
    Signed-off-by: JH Baek <zirubak@gmail.com>

--- main.c
+++ main.c
@@ -56,12 +56,35 @@ void print_list(node *t)
 
 }
 
+node *find_node(int k)
+{
+	node *s;
+	s = head->next; //head 부터 검색을 해야 하니 처음 node를 세팅해 주어야 함
+	while( s->key != k && s != tail)
+	{
+		s = s->next;
+	}
+
+	return s;
+}
+
+node *insert_after(int k, node *t)
+{
+	node *s;
+	s = (node*)malloc(sizeof(node*));
+	s->key = k;
+	s->next = t->next;
+	t->next = s;
+	return s;
+}
+
 int main(void)
 {
 
 	node *t;
 
 	init_list();
+
 	ordered_insert(10);
 	ordered_insert(5);
 	ordered_insert(8);
@@ -72,6 +95,18 @@ int main(void)
 
 	printf("\nInitial Linked list is ");
 	print_list(head->next);
+	printf("\n");
+
+	t = find_node(5);
+	printf("\nFinding 5 is %ssuccessful", t == tail ? "un" : "");
+
+//	t = find_node(0);
+//	printf("\nFinding 0 is %ssuccessful", t == tail ? "un" : "");
+
+	printf("\nInserting 9 after 5");
+	insert_after(9, t);
+	print_list(head->next);
+
 
 	return 0;
 }
